b8668c27ab8088b4006fe6b4702b61d8d3d99b69
Salma Ghariani <salma.ghariani@gmail.com>
2020-03-18 17:47:08 +0100

    "integrer toute la ui template  "

---
+++ template/material-dashboard-angular2-master/e2e/app.po.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+import { browser, element, by } from 'protractor';
+
+export class MaterialDashboardAngularPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
