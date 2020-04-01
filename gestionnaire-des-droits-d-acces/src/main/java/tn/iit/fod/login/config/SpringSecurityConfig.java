9cd314b1a6e26fd8a6997f9d69265f14bea19da7
Amal <33817534+AmalChaari@users.noreply.github.com>
2020-04-01 15:03:11 +0100

    ajouter 2 users statiquement

--- gestionnaire-des-droits-d-acces/src/main/java/tn/iit/fod/login/config/SpringSecurityConfig.java
+++ gestionnaire-des-droits-d-acces/src/main/java/tn/iit/fod/login/config/SpringSecurityConfig.java
@@ -1,10 +1,15 @@
 package tn.iit.fod.login.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -14,8 +19,27 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest()
 				.authenticated().and()
-				// .formLogin().and()
 				.httpBasic();
 	}
-	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+				User.withDefaultPasswordEncoder()
+						.username("user")
+						.password("password")
+						.roles("USER")
+						.build();
+		UserDetails user2 =
+				User.withDefaultPasswordEncoder()
+						.username("user2")
+						.password("password")
+						.roles("USER")
+						.build();
+
+		return new InMemoryUserDetailsManager(user,user2);
+
+	}
+
+
 }
